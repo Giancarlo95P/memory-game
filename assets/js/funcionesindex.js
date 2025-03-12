8a2db38f9958c068829bbb3112c1dca67289edaf
gabriel <gabrielayorap@gmail.com>
2025-03-12 17:15:14 -0500

    Almacenar opciones seleccionadas

--- assets/js/funcionesindex.js
+++ assets/js/funcionesindex.js
@@ -1,10 +1,21 @@
-function activarBoton(botonSeleccionado) {
-    const botones = document.querySelectorAll('.btn'); // Selecciona todos los botones con la clase 'btn'
+function activeButton(botonSeleccionado) {
+    const buttons = document.querySelectorAll('.btn-primary-option'); 
     
-    botones.forEach(boton => {
-        boton.classList.remove('btn-primary-option-select'); // Remueve la clase 'active' de todos
+    buttons.forEach(botonSeleccionado => {
+        botonSeleccionado.classList.remove('btn-primary-option-select'); 
     });
 
-    botonSeleccionado.classList.add('btn-primary-option-select'); // Activa solo el botón seleccionado
+    botonSeleccionado.classList.add('btn-primary-option-select');
 }
 
+function saveOptions() {
+    const tema = document.getElementById('temaSelect').value;
+    const level = document.getElementById('levelSelect').value;
+    const buttonActivated = document.querySelector('.btn-primary-option-select');
+    const gameType = buttonActivated ? buttonActivated.textContent.trim() : "Solo";
+    localStorage.setItem('tema', tema);
+    localStorage.setItem('level', level);
+    localStorage.setItem('modoJuego', gameType);
+    console.log("¡Configuración guardada correctamente!");
+
+}
